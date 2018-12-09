ef14caa6206c9502bd9c46353107572aaa1cc1c3
halation-ghost <wpf_practice@elf-mission.net>
2018-12-09 20:39:16 +0900

    episode:6 用のプロジェクトを追加。

---
+++ 04_episode06/EditorViews/Views/TestPointEditor.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfTestApp.Views
+{
+	/// <summary>
+	/// Interaction logic for TestPointEditor
+	/// </summary>
+	public partial class TestPointEditor : UserControl
+	{
+		public TestPointEditor()
+		{
+			InitializeComponent();
+		}
+	}
+}
