a68b501b0a1b59938c764d8f1ac03f827ae7d7b8
halation-ghost <wpf_practice@elf-mission.net>
2018-12-23 19:27:23 +0900

    EditViewsにバインド用の実装を追加。

--- 05_episode07/EditorViews/ViewModels/PhysicalEditorViewModel.cs
+++ 05_episode07/EditorViews/ViewModels/PhysicalEditorViewModel.cs
@@ -1,16 +1,77 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using Prism.Commands;
+using System;
+using Prism.Mvvm;
+using Prism.Regions;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace WpfTestApp.ViewModels
 {
-	public class PhysicalEditorViewModel : BindableBase
+	/// <summary> 身体測定データの編集画面を表します。 </summary>
+	public class PhysicalEditorViewModel : BindableBase, IDisposable, INavigationAware
 	{
-		public PhysicalEditorViewModel()
-		{
+		#region "プロパティ"
 
+		/// <summary>測定日を取得・設定します。</summary>
+		public ReactiveProperty<DateTime?> MeasurementDate { get; set; }
+
+		/// <summary>身長を取得・設定します。</summary>
+		public ReactiveProperty<double> Height { get; set; }
+
+		/// <summary>体重を取得・設定します。</summary>
+		public ReactiveProperty<double> Weight { get; set; }
+
+		/// <summary>BMIを取得します。</summary>
+		public ReadOnlyReactivePropertySlim<double> Bmi { get; private set; }
+
+		#endregion
+
+		/// <summary>表示するViewを判別します。</summary>
+		/// <param name="navigationContext">Navigation Requestの情報を表すNavigationContext。</param>
+		/// <returns>表示するViewかどうかを表すbool。</returns>
+		bool INavigationAware.IsNavigationTarget(NavigationContext navigationContext){ return true; }
+
+		private PhysicalInformation physical = null;
+		private System.Reactive.Disposables.CompositeDisposable disposables =
+			new System.Reactive.Disposables.CompositeDisposable();
+
+		/// <summary>Viewを表示した後呼び出されます。</summary>
+		/// <param name="navigationContext">Navigation Requestの情報を表すNavigationContext。</param>
+		void INavigationAware.OnNavigatedTo(NavigationContext navigationContext)
+		{
+			if (this.physical != null)
+				return;
+			this.physical = navigationContext.Parameters["TargetData"] as PhysicalInformation;
+
+			this.MeasurementDate = this.physical
+				.ToReactivePropertyAsSynchronized(x => x.MeasurementDate)
+				.AddTo(this.disposables);
+			this.Height = this.physical
+				.ToReactivePropertyAsSynchronized(x => x.Height)
+				.AddTo(this.disposables);
+			this.Weight = this.physical
+				.ToReactivePropertyAsSynchronized(x => x.Height)
+				.AddTo(this.disposables);
+			this.Bmi = this.physical.ObserveProperty(x => x.Bmi)
+				.ToReadOnlyReactivePropertySlim()
+				.AddTo(this.disposables);
+
+			this.RaisePropertyChanged(nameof(this.MeasurementDate));
+			this.RaisePropertyChanged(nameof(this.Height));
+			this.RaisePropertyChanged(nameof(this.Weight));
+			this.RaisePropertyChanged(nameof(this.Bmi));
 		}
+
+		/// <summary>別のViewに切り替わる前に呼び出されます。</summary>
+		/// <param name="navigationContext">Navigation Requestの情報を表すNavigationContext。</param>
+		void INavigationAware.OnNavigatedFrom(NavigationContext navigationContext) { return; }
+
+		/// <summary>コンストラクタ。</summary>
+		public PhysicalEditorViewModel(){ }
+
+		/// <summary>オブジェクトを破棄します。</summary>
+		void IDisposable.Dispose() { this.disposables.Dispose(); }
 	}
 }
--- 05_episode07/EditorViews/ViewModels/TestPointEditorViewModel.cs
+++ 05_episode07/EditorViews/ViewModels/TestPointEditorViewModel.cs
@@ -1,16 +1,84 @@
 ﻿using Prism.Commands;
-using Prism.Mvvm;
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using Prism.Mvvm;
+using Prism.Regions;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
 namespace WpfTestApp.ViewModels
 {
-	public class TestPointEditorViewModel : BindableBase
+	/// <summary> 試験結果データの編集画面を表します。 </summary>
+	public class TestPointEditorViewModel : BindableBase, IDisposable, INavigationAware
 	{
-		public TestPointEditorViewModel()
-		{
+		#region "プロパティ"
 
+		/// <summary>試験日を取得・設定します。</summary>
+		public ReactiveProperty<string> TestDate { get; set; }
+
+		/// <summary>国語の点数を取得・設定します。</summary>
+		public ReactiveProperty<int> JapaneseScore { get; set; }
+
+		/// <summary>数学の点数を取得・設定します。</summary>
+		public ReactiveProperty<int> MathematicsScore { get; set; }
+
+		/// <summary>英語の点数を取得・設定します。</summary>
+		public ReactiveProperty<int> EnglishScore { get; set; }
+
+		/// <summary>平均点を取得します。</summary>
+		public ReadOnlyReactivePropertySlim<double> Average { get; private set; }
+
+		#endregion
+
+		/// <summary>表示するViewを判別します。</summary>
+		/// <param name="navigationContext">Navigation Requestの情報を表すNavigationContext。</param>
+		/// <returns>表示するViewかどうかを表すbool。</returns>
+		bool INavigationAware.IsNavigationTarget(NavigationContext navigationContext){ return true; }
+
+		private TestPointInformation testPoint = null;
+		private System.Reactive.Disposables.CompositeDisposable disposables =
+			new System.Reactive.Disposables.CompositeDisposable();
+
+		/// <summary>Viewを表示した後呼び出されます。</summary>
+		/// <param name="navigationContext">Navigation Requestの情報を表すNavigationContext。</param>
+		void INavigationAware.OnNavigatedTo(NavigationContext navigationContext)
+		{
+			if (this.testPoint != null)
+				return;
+			this.testPoint = navigationContext.Parameters["TargetData"] as TestPointInformation;
+
+			this.TestDate = this.testPoint
+				.ToReactivePropertyAsSynchronized(x => x.TestDate)
+				.AddTo(this.disposables);
+			this.JapaneseScore = this.testPoint
+				.ToReactivePropertyAsSynchronized(x => x.JapaneseScore)
+				.AddTo(this.disposables);
+			this.MathematicsScore = this.testPoint
+				.ToReactivePropertyAsSynchronized(x => x.MathematicsScore)
+				.AddTo(this.disposables);
+			this.EnglishScore = this.testPoint
+				.ToReactivePropertyAsSynchronized(x => x.EnglishScore)
+				.AddTo(this.disposables);
+			this.Average = this.testPoint.ObserveProperty(x => x.Average)
+				.ToReadOnlyReactivePropertySlim()
+				.AddTo(this.disposables);
+
+			this.RaisePropertyChanged(nameof(this.TestDate));
+			this.RaisePropertyChanged(nameof(this.JapaneseScore));
+			this.RaisePropertyChanged(nameof(this.MathematicsScore));
+			this.RaisePropertyChanged(nameof(this.EnglishScore));
+			this.RaisePropertyChanged(nameof(this.Average));
 		}
+
+		/// <summary>別のViewに切り替わる前に呼び出されます。</summary>
+		/// <param name="navigationContext">Navigation Requestの情報を表すNavigationContext。</param>
+		void INavigationAware.OnNavigatedFrom(NavigationContext navigationContext) { return; }
+
+		/// <summary>コンストラクタ。</summary>
+		public TestPointEditorViewModel() { }
+
+		/// <summary>オブジェクトを破棄します。</summary>
+		void IDisposable.Dispose() { this.disposables.Dispose(); }
 	}
 }
