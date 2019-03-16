632e97c1b0bbda33587bfcb1c08285c12314b002
halation-ghost <wpf_practice@elf-mission.net>
2019-03-16 15:48:38 +0900

    自作メッセージボックスを追加。

---
+++ 11_episode13/WpfTestAppServices/Views/QuestionPopup.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WpfTestApp.Views
+{
+	/// <summary>
+	/// Interaction logic for QuestionPopup
+	/// </summary>
+	public partial class QuestionPopup : UserControl
+	{
+		public QuestionPopup()
+		{
+			InitializeComponent();
+		}
+	}
+}
