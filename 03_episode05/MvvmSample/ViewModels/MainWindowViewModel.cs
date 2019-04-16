2fc14a755545f16cb2f048c92f677c75709e1f10
YouseiSakusen <reilo_okita@elf-mission.net>
2019-04-17 00:17:26 +0900

    episode:5 リニューアル用のプロジェクトを追加。

---
+++ 03_episode05/MvvmSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace MvvmSample.ViewModels
+{
+	/// <summary>Personクラス。</summary>
+	public class Person
+	{
+		/// <summary>名前を取得・設定します。</summary>
+		public string Name { get; set; } = string.Empty;
+		/// <summary>誕生日を取得・設定します。</summary>
+		public DateTime? Birthday { get; set; } = null;
+	}
+
+	/// <summary>ユーザの読込と保存を行う。</summary>
+	public static class UserAgent
+	{
+		/// <summary>ユーザデータを読み込む。</summary>
+		/// <returns>読み込んだユーザを表すPerson。</returns>
+		public static Person Load() //{ return new Person(); }
+		{
+			return new Person()
+			{
+			Birthday = DateTime.Parse("1980/1/1"),
+				Name = "沖田玲郎"
+			};
+		}
+
+	/// <summary>ユーザデータを保存する。</summary>
+	/// <param name="target">保存するユーザを表すPerson。</param>
+	public static void Save(Person target) { return; }
+	}
+
+	/// <summary>Prism Shell の VM。</summary>
+	public class MainWindowViewModel : BindableBase
+	{
+		private string userName = string.Empty;
+		/// <summary>ユーザ名を取得・設定します。</summary>
+		public string UserName
+		{
+			get { return userName; }
+			set { SetProperty(ref userName, value); }
+		}
+
+		private DateTime? userbirth = null;
+		/// <summary>ユーザの誕生日を取得・設定します。</summary>
+		public DateTime? UserBirthDay
+		{
+			get { return userbirth; }
+			set
+			{
+				SetProperty(ref userbirth, value);
+
+				if (userbirth.HasValue)
+				{
+					var today = DateTime.Now.Date;
+					var year = today.Year - this.userbirth.Value.Year;
+					if (today.AddDays(-year) < this.userbirth)
+						year--;
+
+					this.UserAge = year;
+				}
+			}
+		}
+
+		private int age;
+		/// <summary>ユーザの年齢を取得します。</summary>
+		public int UserAge
+		{
+			get { return age; }
+			private set { SetProperty(ref age, value); }
+		}
+
+		private DelegateCommand saveCmd;
+		/// <summary>ユーザの保存コマンド。</summary>
+		public DelegateCommand SaveCommand => 
+			saveCmd ?? (saveCmd = new DelegateCommand(ExecuteSaveCommand, CanExecuteSaveCommand));
+
+		/// <summary>ユーザの保存を実行します。</summary>
+		void ExecuteSaveCommand()
+		{
+			var user = new Person()
+			{
+				Birthday = this.UserBirthDay,
+				Name = this.UserName
+			};
+
+			UserAgent.Save(user);
+		}
+
+		/// <summary>ユーザの保存ボタンの実行可否を取得・設定します。</summary>
+		/// <returns>ユーザの保存ボタンの実行可否を表すbool。</returns>
+		bool CanExecuteSaveCommand()
+		{
+			return true;
+		}
+
+		/// <summary>コンストラクタ。</summary>
+		public MainWindowViewModel()
+		{
+			var user = UserAgent.Load();
+
+			this.userName = user.Name;
+			this.UserBirthDay = user.Birthday;
+		}
+	}
+}
---
+++ 03_episode05/NavigationTree/ViewModels/EpisodeSampleViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System.Collections.ObjectModel;
+
+namespace WpfTestApp.ViewModels
+{
+	public class EpisodeSampleViewModel : BindableBase
+	{
+		// ReadOnly無しのReactiveProperty
+		public ReactiveProperty<string> Name { get; set; }
+		public ReactiveCollection<int> Numbers { get; }
+
+		// ReadOnly付きのReactiveProperty
+		public ReadOnlyReactiveProperty<string> NickName { get; }
+		public ReadOnlyReactiveCollection<PersonalInformation> Persons { get; }
+		public ReadOnlyReactiveCollection<PhysicalInformation> PhysicalInformations { get; }
+
+		private string nickNameSource = string.Empty;
+		private ObservableCollection<PersonalInformation> personList = null;
+		private ReactiveCollection<PhysicalInformation> physicals { get; set; }
+
+		public EpisodeSampleViewModel()
+		{
+			// ReadOnlyでないReactivePropertyは変数のように扱える
+			this.Name = new ReactiveProperty<string>(string.Empty);
+			this.Numbers = new ReactiveCollection<int>() { 0, 1, 2 };
+
+			// ReadOnlyのReactivePropertyは必ずソースオブジェクトが必要
+			this.NickName = this.ObserveProperty(x => x.nickNameSource)
+				.ToReadOnlyReactiveProperty();
+
+			// つまり、以下のような初期化はできない
+			//this.NickName = new ReadOnlyReactiveProperty<string>("テスト");
+
+
+			// この場合の初期化時パラメータにはIObservable<string>が必要と言うエラーになるので
+			// ソースにする変数は必須。
+
+			// ReadOnlyReactiveCollectionのソースにはObservableCollectionか
+			// ReactiveCollectionから作成するのが楽。
+			this.personList = new ObservableCollection<PersonalInformation>();
+			this.personList.Add(new PersonalInformation() { Name = "テスト1" });
+			this.personList.Add(new PersonalInformation() { Name = "テスト2" });
+			this.personList.Add(new PersonalInformation() { Name = "テスト3" });
+
+			this.Persons = this.personList
+				.ToReadOnlyReactiveCollection();
+
+			this.physicals = new ReactiveCollection<PhysicalInformation>();
+			this.physicals.Add(new PhysicalInformation() { Id = 0 });
+			this.physicals.Add(new PhysicalInformation() { Id = 1 });
+
+			this.PhysicalInformations = this.physicals
+				.ToReadOnlyReactiveCollection();
+		}
+	}
+}
